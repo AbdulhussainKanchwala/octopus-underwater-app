8c89be3881d147297b1f686b846d90309db141c5
AbdulhussainKanchwala <abdulhussain.kanchwala@emsbilling.com>
2022-08-17 15:59:01 +0530

    testing env var

--- ClientApp/src/environments/environment.prod.ts
+++ ClientApp/src/environments/environment.prod.ts
@@ -1,10 +1,9 @@
 export const environment = {
   production: true,
-  SSOlogin: 'https://emsmart20.dev.emsbilling.com/login',
-  SSOlogout: 'https://account.prod-ac.emsbilling.com/site/landing-page',
-  EmsAdminPortalApi: 'https://api-admin-portal.prod.emsbilling.com/',
-  WebUIProjectURL: 'https://google.com?env=prod',
-  WebAPIProjectURL: "",
-  testVariables: "testVariables123"
+  SSOlogin: "envSSOlogin",
+  SSOlogout: "envSSOlogout",
+  EmsAdminPortalApi: "envEmsAdminPortalApi",
+  WebUIProjectURL: "envWebUIProjectURL",
+  WebAPIProjectURL: "envWebAPIProjectURL",
 };
 
--- ClientApp/src/environments/test.sh
+++ ClientApp/src/environments/test.sh
@@ -1,4 +1,9 @@
 echo ${testVariables}
 echo $testVariables
 echo $production
-sed -i -e "s/testVariables123/${testVariables}/g" src/environments/environment.prod.ts
+#sed -i -e "s/testVariables123/${production}/g" src/environments/environment.prod.ts
+sed -i -e "s/envSSOlogin/${SSOlogin}/g" src/environments/environment.prod.ts
+sed -i -e "s/envSSOlogout/${SSOlogout}/g" src/environments/environment.prod.ts
+sed -i -e "s/envEmsAdminPortalApi/${EmsAdminPortalApi}/g" src/environments/environment.prod.ts
+sed -i -e "s/envWebUIProjectURL/${WebUIProjectURL}/g" src/environments/environment.prod.ts
+sed -i -e "s/envWebAPIProjectURL/${WebAPIProjectURL}/g" src/environments/environment.prod.ts
