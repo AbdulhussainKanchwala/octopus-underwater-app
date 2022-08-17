cde5e65135e8bce32b14176f0cde336f9641728d
AbdulhussainKanchwala <abdulhussain.kanchwala@emsbilling.com>
2022-08-17 17:59:33 +0530

    font-

--- ClientApp/src/environments/envset.sh
+++ ClientApp/src/environments/envset.sh
@@ -1,6 +1,3 @@
-echo ${testVariables}
-echo $testVariables
-echo $production
 #sed -i -e "s/envtestVariables/$testVariables/g" src/environments/environment.prod.ts
 #sed -i -e "s/envSSOlogin/$SSOlogin/g" src/environments/environment.prod.ts
 #sed -i -e "s/envSSOlogout/$SSOlogout/g" src/environments/environment.prod.ts
@@ -11,3 +8,4 @@ sed -i -e 's@envSSOlogin@'"$SSOlogin"'@' src/environments/environment.prod.ts
 sed -i -e 's@envSSOlogout@'"$SSOlogout"'@' src/environments/environment.prod.ts
 sed -i -e 's@envEmsAdminPortalApi@'"$EmsAdminPortalApi"'@' src/environments/environment.prod.ts
 sed -i -e 's@envWebUIProjectURL@'"$WebUIProjectURL"'@' src/environments/environment.prod.ts
+sed -i -e 's@envFontawesomeToken@'"$FontawesomeToken"'@' /var/jenkins_home/.npmrc
