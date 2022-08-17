2207d585850c08cf19f5a089591a992c94749f4d
AbdulhussainKanchwala <abdulhussain.kanchwala@emsbilling.com>
2022-08-17 16:24:36 +0530

    comma change

--- ClientApp/src/environments/test.sh
+++ ClientApp/src/environments/test.sh
@@ -1,8 +1,8 @@
 echo ${testVariables}
 echo $testVariables
 echo $production
-sed -i -e "s/envtestVariables/${testVariables}/g" src/environments/environment.prod.ts
-sed -i -e "s/envSSOlogin/${SSOlogin}/g" src/environments/environment.prod.ts
-sed -i -e "s/envSSOlogout/${SSOlogout}/g" src/environments/environment.prod.ts
-sed -i -e "s/envEmsAdminPortalApi/${EmsAdminPortalApi}/g" src/environments/environment.prod.ts
-sed -i -e "s/envWebUIProjectURL/${WebUIProjectURL}/g" src/environments/environment.prod.ts
+sed -i -e 's/envtestVariables/${testVariables}/g' src/environments/environment.prod.ts
+sed -i -e 's/envSSOlogin/${SSOlogin}/g' src/environments/environment.prod.ts
+sed -i -e 's/envSSOlogout/${SSOlogout}/g' src/environments/environment.prod.ts
+sed -i -e 's/envEmsAdminPortalApi/${EmsAdminPortalApi}/g' src/environments/environment.prod.ts
+sed -i -e 's/envWebUIProjectURL/${WebUIProjectURL}/g' src/environments/environment.prod.ts
