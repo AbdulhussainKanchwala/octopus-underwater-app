5985566d0a20ae5fc9ffbf0e84bcc4099a322675
AbdulhussainKanchwala <abdulhussain.kanchwala@emsbilling.com>
2022-08-17 16:57:08 +0530

    mistake

--- ClientApp/src/environments/test.sh
+++ ClientApp/src/environments/test.sh
@@ -1,8 +1,13 @@
 echo ${testVariables}
 echo $testVariables
 echo $production
-sed -i -e "s/envtestVariables/$testVariables/g" src/environments/environment.prod.ts
-sed -i -e "s/envSSOlogin/$SSOlogin/g" src/environments/environment.prod.ts
-sed -i -e "s/envSSOlogout/$SSOlogout/g" src/environments/environment.prod.ts
-sed -i -e "s/envEmsAdminPortalApi/$EmsAdminPortalApi/g" src/environments/environment.prod.ts
-sed -i -e "s/envWebUIProjectURL/$WebUIProjectURL/g" src/environments/environment.prod.ts
+#sed -i -e "s/envtestVariables/$testVariables/g" src/environments/environment.prod.ts
+#sed -i -e "s/envSSOlogin/$SSOlogin/g" src/environments/environment.prod.ts
+#sed -i -e "s/envSSOlogout/$SSOlogout/g" src/environments/environment.prod.ts
+#sed -i -e "s/envEmsAdminPortalApi/$EmsAdminPortalApi/g" src/environments/environment.prod.ts
+#sed -i -e "s/envWebUIProjectURL/$WebUIProjectURL/g" src/environments/environment.prod.ts
+sed -i -e 's@envtestVariables@'"$testVariables"'@' src/environments/environment.prod.ts
+sed -i -e 's@envSSOlogin@'"$SSOlogin"'@' src/environments/environment.prod.ts
+sed -i -e 's@envSSOlogout@'"$SSOlogout"'@' src/environments/environment.prod.ts
+sed -i -e 's@envEmsAdminPortalApi@'"$EmsAdminPortalApi"'@' src/environments/environment.prod.ts
+sed -i -e 's@envWebUIProjectURL@'"$WebUIProjectURL"'@' src/environments/environment.prod.ts
